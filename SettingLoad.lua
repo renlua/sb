a69e8b9b21f871106cbdbbfbd7ae6f16d6f53449
bailib <littlebaiw@qq.com>
2025-04-08 13:09:56 +0800

    Create SettingLoad.lua

---
+++ SettingLoad.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [116495829188952] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/Dead-RailV2.lua", -- Dead Rails
+  [70876832253163] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/Dead-RailV2.lua", -- Dead Rails
+  [18687417158] = "https://github.com/XiaoyeQWQ/VEP/raw/refs/heads/main/Forsaken.lua", -- Forsaken
+}
+return Games
