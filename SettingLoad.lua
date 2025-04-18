d8bb036edd6f7bc8567940c51c6498781782a163
bailib <littlebaiw@qq.com>
2025-04-18 18:21:06 +0800

    Update SettingLoad.lua

--- SettingLoad.lua
+++ SettingLoad.lua
@@ -2,5 +2,6 @@ local Games = {
   [116495829188952] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/Dead-RailV2.lua", -- Dead Rails
   [70876832253163] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/Dead-RailV2.lua", -- Dead Rails
   [18687417158] = "https://github.com/XiaoyeQWQ/VEP/raw/refs/heads/main/Forsaken.lua", -- Forsaken
+  [4639625707] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/WarTycoon.txt",
 }
 return Games
