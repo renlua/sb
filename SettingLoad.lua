2d4f8eb21e21676918d28ad37b80c2699f21f75b
bailib <littlebaiw@qq.com>
2025-04-20 15:23:30 +0800

    Update SettingLoad.lua

--- SettingLoad.lua
+++ SettingLoad.lua
@@ -2,6 +2,7 @@ local Games = {
   [116495829188952] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/Dead-RailV2.lua", -- Dead Rails
   [70876832253163] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/Dead-RailV2.lua", -- Dead Rails
   [18687417158] = "https://github.com/XiaoyeQWQ/VEP/raw/refs/heads/main/Forsaken.lua", -- Forsaken
-  [4639625707] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/WarTycoon.txt",
+  [4639625707] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/WarTycoon.txt",--WarTycoon
+  [2210085102] = "https://github.com/XiaoyeQWQ/game/raw/refs/heads/main/Rise-of-the-Legend-Hub-Naval-Warfare.lucky"--NavalWarfare
 }
 return Games
