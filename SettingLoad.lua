f31b3d4163416ae9824b762372a38ad7a867bf36
bailib <littlebaiw@qq.com>
2025-05-04 09:29:42 +0800

    Update SettingLoad.lua

--- SettingLoad.lua
+++ SettingLoad.lua
@@ -5,6 +5,8 @@ local Games = {
   [4639625707] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/WarTycoon.txt",--WarTycoon
   [2210085102] = "https://github.com/XiaoyeQWQ/game/raw/refs/heads/main/Rise-of-the-Legend-Hub-Naval-Warfare.lucky",--NavalWarfare
   [126884695634066] = "https://github.com/bailib/Roblox/raw/refs/heads/main/GrowAGarden.txt",--Grow a garden
-  [121308443347459] = "https://github.com/bailib/Roblox/raw/refs/heads/main/7days.lua"
+  [121308443347459] = "https://github.com/bailib/Roblox/raw/refs/heads/main/7days.lua",--7DAYS
+  [12137249458] = "https://github.com/bailib/Roblox/raw/refs/heads/main/GunFFA.lua",--GunFFa
+  [4618049391] = "https://github.com/bailib/Roblox/raw/refs/heads/main/HeBeiTangCountry.lua"
 }
 return Games
