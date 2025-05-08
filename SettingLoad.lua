6919e7ba02e10c99b3f2b360e9d970b91f6e047d
bailib <littlebaiw@qq.com>
2025-05-08 21:51:10 +0800

    Update SettingLoad.lua

--- SettingLoad.lua
+++ SettingLoad.lua
@@ -1,12 +1,11 @@
 local Games = {
-  [116495829188952] = "https://github.com/bailib/Roblox/raw/refs/heads/main/Dead-RailV5.lua", -- Dead Rails
-  [70876832253163] = "https://github.com/bailib/Roblox/raw/refs/heads/main/Dead-RailV5.lua", -- Dead Rails
-  [18687417158] = "https://github.com/XiaoyeQWQ/VEP/raw/refs/heads/main/Forsaken.lua", -- Forsaken
-  [4639625707] = "https://github.com/XiaoyeQWQ/state/raw/refs/heads/main/WarTycoon.txt",--WarTycoon
-  [2210085102] = "https://github.com/XiaoyeQWQ/game/raw/refs/heads/main/Rise-of-the-Legend-Hub-Naval-Warfare.lucky",--NavalWarfare
-  [126884695634066] = "https://github.com/bailib/Roblox/raw/refs/heads/main/GrowAGarden.txt",--Grow a garden
-  [121308443347459] = "https://github.com/bailib/Roblox/raw/refs/heads/main/7days.lua",--7DAYS
-  [12137249458] = "https://github.com/bailib/Roblox/raw/refs/heads/main/GunFFA.lua",--GunFFa
-  [4618049391] = "https://github.com/bailib/Roblox/raw/refs/heads/main/HeBeiTangCountry.lua",
+  [116495829188952] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/Dead-RailV5.lua", -- Dead Rails
+  [70876832253163] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/Dead-RailV5.lua", -- Dead Rails
+  [18687417158] = "https://raw.githubusercontent.com/XiaoyeQWQ/VEP/refs/heads/main/Forsaken.lua", -- Forsaken
+  [2210085102] = "https://raw.githubusercontent.com/XiaoyeQWQ/game/refs/heads/main/Rise-of-the-Legend-Hub-Naval-Warfare.lucky",--NavalWarfare
+  [126884695634066] = "",--Grow a garden
+  [121308443347459] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/7days.lua",--7DAYS
+  [12137249458] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/GunFFA.lua",--GunFFa
+  [4618049391] = "https://raw.githubusercontent.com/bailib/Roblox/refs/heads/main/HeBeiTangCountry.lua",
 }
 return Games
